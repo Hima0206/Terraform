7f9966072bcdbbe6ef3a956f086da85be0bf55e6
Hima0206 <Himateja0206@gmail.com>
2025-12-22 16:03:39 +0530

    Update providers.tf

--- TFWS/providers.tf
+++ TFWS/providers.tf
@@ -1,9 +1,12 @@
 provider "aws" {
   region = var.aws_region
   profile = "default"
+  access_key = AKIAXVL5GZ227AI3HYDB
+  secret_key = D/G2nyTE6F2s3qSv/NvyMLGXlb1W2pc4KTjzxwh
 }
 
 # This provider block specifies the AWS region to use for deploying resources.
 # The region is set using a variable called "aws_region", which can be defined in a separate variables file or passed in as an argument when running Terraform.
+
 
 
