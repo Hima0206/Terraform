738aa3506d644c85fbf6f78c6e4f92541d39ee57
Hima0206 <Himateja0206@gmail.com>
2025-12-23 09:33:53 +0530

    Update AWS provider with new access keys
    Updated AWS provider configuration with new access keys.

--- TFWS/providers.tf
+++ TFWS/providers.tf
@@ -1,12 +1,15 @@
 provider "aws" {
   region = var.aws_region
   profile = "default"
-  AWS_SECRET_ACCESS_KEY=AKIA_TEST_123456
+  AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7XEAPMEL"
+  AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
 
 }
 
 # This provider block specifies the AWS region to use for deploying resources.
 # The region is set using a variable called "aws_region", which can be defined in a separate variables file or passed in as an argument when running Terraform.
+
 
 
 
